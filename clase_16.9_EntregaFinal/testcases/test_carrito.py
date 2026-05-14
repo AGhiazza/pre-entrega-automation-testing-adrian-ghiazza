beecc51eb506cca0a788446ee05994de9c3e9667
Adrian Ghiazza <tu-email@gmail.com>
2026-05-14 16:32:01 -0300

    docs: se agregaron y refinaron comentarios en test_inventario y test_carrito

--- clase_16.9_EntregaFinal/testcases/test_carrito.py
+++ clase_16.9_EntregaFinal/testcases/test_carrito.py
@@ -1,35 +1,35 @@
 from selenium.webdriver.common.by import By
 from funcAux.InventoryPage import agregar_al_carrito, ir_al_carrito
 
-def test_CA01_cart_badge(driver_logged):
-    agregar_al_carrito(driver_logged, 0) #llama a la función agregar_al_carrito y le dá el indice 0 para decirle que busque el primer producto
-    carritoBadge = driver_logged.find_element(By.CLASS_NAME, "shopping_cart_badge")
+def test_CA01_cart_badge(driver_logged):    #Caso para verificar que se muestre el numero de productos cuando se agrega al menos 1 al carrito
+    agregar_al_carrito(driver_logged, 0)    #llama a la función agregar_al_carrito y le dá el indice 0 para decirle que busque el primer producto
+    carritoBadge = driver_logged.find_element(By.CLASS_NAME, "shopping_cart_badge") #busca el elemento de la marca de cantidad de prodcutos en el carrito y lo guarda en la variable
     assert carritoBadge.is_displayed(), "No se visualiza badge del carrito"
 
-def test_CA02_cart_badge_increase(driver_logged):
+def test_CA02_cart_badge_increase(driver_logged):   #Caso para agregar 2 productos al carrito y ver que el contador suba.
     agregar_al_carrito(driver_logged, 0)
     agregar_al_carrito(driver_logged, 1)
     carritoBadge = driver_logged.find_element(By.CLASS_NAME, "shopping_cart_badge")
-    carritoBadgeNum = int(carritoBadge.text)
-    print(f"Cantidad de Productos en el carrito: {carritoBadgeNum}")
-    assert carritoBadgeNum == 2, "No se visualiza la cantidad correcta de productos en el icono del carrito"
+    carritoBadgeNum = int(carritoBadge.text)    #Toma el texto en la variable del contador del carrito y lo convierte en integer para despues comparar
+    print(f"Cantidad de Productos en el carrito: {carritoBadgeNum}")    #Imprime por consola la cantidad de productos agregados
+    assert carritoBadgeNum == 2, "No se visualiza la cantidad correcta de productos en el icono del carrito"    #valida que la cantidad de prodcutos sea igual a 2
 
-def test_CA03_go_to_cart(driver_logged):
+def test_CA03_go_to_cart(driver_logged):    #Caso para validar que cuando se navega al carrito se visualice la pagina correcta.
     ir_al_carrito(driver_logged)
-    assert "/cart.html" in driver_logged.current_url, "No se visualiza la pagina del carrito"
+    assert "/cart.html" in driver_logged.current_url, "No se visualiza la pagina del carrito"   
 
-def test_CA04_check_item_in_cart(driver_logged):
+def test_CA04_check_item_in_cart(driver_logged):    #Caso para validar que las caracteristicas de un producto sean iguales en el inventario como en el carrito
     productos = driver_logged.find_elements(By.CLASS_NAME, "inventory_item")
     primerProducto = productos [0]
-    nombreInv = str(primerProducto.find_element(By.CLASS_NAME, "inventory_item_name").text)
-    precioInv = str(primerProducto.find_element(By.CLASS_NAME, "inventory_item_price").text)
-    descrpInv = str(primerProducto.find_element(By.CLASS_NAME, "inventory_item_desc").text)
+    nombreInv = str(primerProducto.find_element(By.CLASS_NAME, "inventory_item_name").text) #Guarda el nombre del producto en el inventario.
+    precioInv = str(primerProducto.find_element(By.CLASS_NAME, "inventory_item_price").text) #Guarda el precio del producto en el inventario.
+    descrpInv = str(primerProducto.find_element(By.CLASS_NAME, "inventory_item_desc").text) #Guarda la descripción del producto en el inventario.
     agregar_al_carrito(driver_logged, 0)
     ir_al_carrito(driver_logged)
-    productoEnCarrito = driver_logged.find_element(By.CLASS_NAME, "cart_item")
-    nombreCar = str(productoEnCarrito.find_element(By.CLASS_NAME, "inventory_item_name").text)
+    productoEnCarrito = driver_logged.find_element(By.CLASS_NAME, "cart_item")  #Busca la tarjeta del producto dentro del carrito.
+    nombreCar = str(productoEnCarrito.find_element(By.CLASS_NAME, "inventory_item_name").text)  #Guarda el nombre del producto en el carrito.
     precioCar = str(productoEnCarrito.find_element(By.CLASS_NAME, "inventory_item_price").text)
     descrpCar = str(productoEnCarrito.find_element(By.CLASS_NAME, "inventory_item_desc").text)
-    assert nombreInv == nombreCar, "El nombre del producto en el catálogo difiere al del carrito"
+    assert nombreInv == nombreCar, "El nombre del producto en el catálogo difiere al del carrito"   #Compara los nombres del producto fuera en el invetorio contra los del carrito
     assert precioInv == precioCar, "El precio del producto en el catálogo difiere al del carrito"
     assert descrpInv == descrpCar, "La descripción del producto en el catálogo difiere a la del carrito"
--- clase_16.9_EntregaFinal/testcases/test_inventario.py
+++ clase_16.9_EntregaFinal/testcases/test_inventario.py
@@ -1,27 +1,28 @@
 from selenium.webdriver.common.by import By
 
 
-def test_IN01_title_validation(driver_logged):
-    titulo = driver_logged.title
-    assert "Swag Labs" in titulo, "El título de la página es incorrecto."
+def test_IN01_title_validation(driver_logged):  #Caso para validar el titulo de la pagina
+    titulo = driver_logged.title                #.title obtiene el título de la pestaña del navegador y en este caso lo guarda en la variable "titulo"
+    assert "Swag Labs" in titulo, "El título de la página es incorrecto." #verifica que "Swag Labs" esté contenido en el titulo
 
-def test_IN02_prods_in_catalog(driver_logged):
-    productos = driver_logged.find_elements(By.CLASS_NAME, "inventory_item")
-    assert len(productos) > 0, "No se visualizan productos en la pagina."
+def test_IN02_prods_in_catalog(driver_logged):  #Caso para validar que se vean productos
+    productos = driver_logged.find_elements(By.CLASS_NAME, "inventory_item") #Busca todos los elementos con la clase "inventory_item"
+    assert len(productos) > 0, "No se visualizan productos en la pagina." #verifica que la cantidad de resultados de la busqueda sea mayor a 0
 
-def test_IN03_ui_validation(driver_logged):
-    hamburguesa = driver_logged.find_element(By.ID, "react-burger-menu-btn")
+def test_IN03_ui_validation(driver_logged):     #Caso para buscar diversos elementos en la UI y verificar que sean visibles
+    hamburguesa = driver_logged.find_element(By.ID, "react-burger-menu-btn")    #a diferencia de elementS que hace una lista de todos los elementos que encuentre, 
+                                                                                #find_element guarda el primer elemento que encuentra directamente
     filtro = driver_logged.find_element(By.CLASS_NAME, "product_sort_container")
     carrito = driver_logged.find_element(By.CLASS_NAME, "shopping_cart_link")
-    assert hamburguesa.is_displayed() , "No se visualiza el icono hamburguesa"
+    assert hamburguesa.is_displayed() , "No se visualiza el icono hamburguesa"  #se usa el metodo .is_displayed() para verificar que el elemento sea visible en la pagina
     assert filtro.is_displayed(), "No se visualiza el icono de filtro"
     assert carrito.is_displayed(), "No se visualiza el icono de carrito"
 
-def test_IN04_product_validation(driver_logged):
-    productos = driver_logged.find_elements(By.CLASS_NAME, "inventory_item")
-    primerProducto = productos [0]
+def test_IN04_product_validation(driver_logged):    #Caso para validar que el nombre y precio del producto se visualice (tomando el primer producto que encuentre)
+    productos = driver_logged.find_elements(By.CLASS_NAME, "inventory_item") #Busca todos los elementos con la clase "inventory_item"
+    primerProducto = productos [0]  #selecciona el primer elemento de la lista "productos" y lo guarda en "primerProducto"
     nombre = primerProducto.find_element(By.CLASS_NAME, "inventory_item_name")
     precio = primerProducto.find_element(By.CLASS_NAME, "inventory_item_price")
-    print(f"Nombre: {nombre.text} | Precio: {precio.text}")
+    print(f"Nombre: {nombre.text} | Precio: {precio.text}") #imprime los valores recuperados para nombre y precio con la variable .text
     assert nombre.is_displayed(), "No se visualiza el nombre del primer producto"
     assert precio.is_displayed(), "No se visualiza el precio del primer producto"
