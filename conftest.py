e03241f1f47f7a02de34d8d674f2f8aaa01d2c5c
Adrian Ghiazza <tu-email@gmail.com>
2026-06-19 16:05:27 -0300

    refactor: Se modificó conftest con opcionales para Github Actions (disable GPU, remote debugging, etc)

--- conftest.py
+++ conftest.py
@@ -20,6 +20,11 @@ def driver():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
 
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-infobars")
+
     driver = webdriver.Chrome(options=options) #abre chrome con las opciones configuradas que se guardaron antes y se guarda todo en la variable driver.
                                                #la variable driver es la que usan los tests para interactuar con el navegador.
 
