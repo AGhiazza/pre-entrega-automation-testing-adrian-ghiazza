0356f6fe4ff4567ece70395a64f83c5c367ac88d
Adrian Ghiazza <tu-email@gmail.com>
2026-06-19 15:38:05 -0300

    feat: Se completó implementación de Behave/Gherkin

--- features/steps/login_steps.py
+++ features/steps/login_steps.py
@@ -1,3 +1,29 @@
+from pages.LoginPage import LoginPage
+from behave import given, when, then
 
+@given("el usuario está en la página de login")
+def step_abrir_login(context):
+    context.login_page = LoginPage(context.driver)
+    context.login_page.abrir_pagina()
 
-@given("")
+@when("ingresa usuario '{usuario}' y contraseña '{contrasena}'")
+def step_ingresar_credenciales(context,usuario,contrasena):
+    if usuario == "VACIO":
+        usuario = ""
+    if contrasena == "VACIO":
+        contrasena = ""
+    context.login_page.ingresar_usuario(usuario)
+    context.login_page.ingresar_contrasena(contrasena)
+
+@when("hace click en boton login")
+def step_click_login(context):
+    context.login_page.click_login()
+
+@then("debería ver la página de inventario")
+def step_validar_login_exitoso(context):
+    assert "/inventory.html" in context.driver.current_url, "No se visualiza la pagina del inventario"
+
+@then("debería ver el error '{mensaje}'")
+def step_validar_error_login(context, mensaje):
+    error = context.login_page.mensaje_error()
+    assert mensaje in error, f"Mensaje de error esperado: {mensaje} | Recibido: {error}"
