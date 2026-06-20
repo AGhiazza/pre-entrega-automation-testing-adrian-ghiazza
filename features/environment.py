a2ad8c0b80d5473453099adbe825d0c63e262bc6
Adrian Ghiazza <tu-email@gmail.com>
2026-06-20 18:01:06 -0300

    refactor: Se modificó environment de behave con opciones para Github Actions

--- features/environment.py
+++ features/environment.py
@@ -4,6 +4,9 @@ def before_scenario(context, scenario):
     # Se ejecuta antes de cada escenario
     options = webdriver.ChromeOptions()
     options.add_argument("--incognito")
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+
     context.driver = webdriver.Chrome(options=options)
 
 def after_scenario(context, scenario):
